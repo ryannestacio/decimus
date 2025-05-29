b84d59f5d43a5c32aaf01e96b7a40a89f8ac253d
ryannestacio <ryan.estacio.98@gmail.com>
2025-05-28 23:38:02 -0300

    feat: adiconado o meu projeto ao console do Firebase para autenticação do usuário

--- lib/main.dart
+++ lib/main.dart
@@ -6,8 +6,13 @@ import 'package:decimus/screens/recebiveis.dart';
 import 'package:flutter/material.dart';
 import 'screens/login.dart';
 import 'screens/home.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(const MyApp());
   runApp(const MyApp());
 }
 
