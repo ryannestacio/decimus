68bc16dfdbf6957d7734e18ecab671c3e75762c6
ryannestacio <ryan.estacio.98@gmail.com>
2025-08-08 14:18:52 -0300

    Adicionado as animações com a biblioteca page_transition (https://pub.dev/packages/page_transition) em todas as telas

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ import 'screens/login.dart';
 import 'screens/home.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'package:page_transition/page_transition.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -25,14 +26,54 @@ class MyApp extends StatelessWidget {
 
       initialRoute: '/login',
 
-      routes: {
+      onGenerateRoute: (settings) {
+        switch (settings.name) {
+          case '/':
+            return MaterialPageRoute(builder: (context) => const LoginScreen());
+          case '/home':
+            return PageTransition(
+              type: PageTransitionType.fade, // Escolha o tipo de transição
+              child: const HomeScreen(),
+            );
+          case '/recebiveis':
+            return PageTransition(
+              type: PageTransitionType.fade,
+              child: const RecebiveisScreen(),
+            );
+          case '/despesas':
+            return PageTransition(
+              type: PageTransitionType.fade,
+              child: const DespesasScreen(),
+            );
+          case '/devedores':
+            return PageTransition(
+              type: PageTransitionType.fade,
+              child: const DevedoresScreen(),
+            );
+          case '/caixa':
+            return PageTransition(
+              type: PageTransitionType.fade,
+              child: const CaixaScreen(),
+            );
+          case '/login':
+            return PageTransition(
+              type: PageTransitionType.bottomToTop,
+              child: const LoginScreen(),
+              duration: Duration(milliseconds: 550),
+            );
+          default:
+            return null;
+        }
+      },
+
+      /*routes: {
         '/login': (context) => LoginScreen(),
         '/home': (context) => HomeScreen(),
         '/recebiveis': (context) => RecebiveisScreen(),
         '/despesas': (context) => DespesasScreen(),
         '/devedores': (context) => DevedoresScreen(),
         '/caixa': (context) => CaixaScreen(),
-      },
+      },*/
     );
   }
 }
